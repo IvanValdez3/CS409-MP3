b4701a226284d607d1a34fe90c4acdbdc040bcfe
unknown <LocalAdmin@ENGRIT-LOAN-F02.ad.uillinois.edu>
2025-11-04 19:55:29 -0600

    finished creating the routes for the api calls on users and tasks. Simply defining their end points and will now work on the controllers. This MP has a strucutre of MVC

---
+++ routes/tasks.js
@@ -0,0 +1,14 @@
+// The task route should handle all endpoints related to tasks
+const express = require('express');
+
+// For Task we need 5 different actions
+const { getTasks, postTask, getTaskId, putTask, deleteTask } = require('to be created');
+
+modeule.exports = function (router) {
+    router.get('/tasks', getTasks);
+    router.post('/tasks', postTask);
+    router.get('/tasks/:taskId', getTaskId);
+    router.put('/tasks/:taskId', putTask);
+    router.delete('/tasks/:taskId', deleteTask);   
+    return router;
+}
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+//Similar to tasks.js, this file sets up user-related routes
+const express = require('express');
+
+// For User we need 5 different actions
+const { getUsers, postUsers, getUserId, putUser, deleteUser } = require('to be created');
+
+module.exports = function (router) {
+    router.get('/users', getUsers);
+    router.post('/users', postUsers);
+    router.get('/users/:userId', getUserId);
+    router.put('/tasks/:taskId', putTask);
+    router.delete('/tasks/:taskId', deleteTask);   
+    return router;
+}
