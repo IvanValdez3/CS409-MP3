4bf1b87fe0b8b3b3aa7380567eb7fec64463662e
unknown <LocalAdmin@ENGRIT-LOAN-F02.ad.uillinois.edu>
2025-11-04 22:20:44 -0600

    Looked over some useful resources attached in the code snippet at the top. Essentially I created a helper parser function for the purpose of the JSON encoded query strings and the fact that tasks and user will both use this so it just made sense. Finished getTask and Post Tasks. Will continue to test and implement as I go

---
+++ controllers/parseHelper.js
@@ -0,0 +1,31 @@
+function parseHelper(request) {
+    // This helper function will serve for
+    // ": In addition, the API has the following JSON encoded query string parameters for the GET requests to the users and tasks endpoints."
+
+    // While it's possible to simply do it in the controller get calls since we will use it in both tasks and users, we can simply
+    // Truncate code and not have to check each param
+
+    //Essentially we're converting strings to actual objects
+
+    // 1. where 2. sort 3. select 4. skip 5. limit 6. count
+    //Useful links:
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+    //https://www.tutscoder.com/post/nodejs-filtering-sorting-pagination
+
+    // query will define which document to fetch
+    const query = request.query.where ? JSON.parse(request.query.where) : {};
+   
+    // options will define how to fetch the document
+    const query_options = {
+        sort: request.query.sort ? JSON.parse(request.query.sort) : {},
+        select: request.query.select ? JSON.parse(request.query.select) : {},
+        skip: request.query.skip ? parseInt(request.query.skip, 10) : 0,
+        limit: request.query.limit ? parseInt(request.query.limit, 10) : 100,
+    };
+
+    const count = request.query.count === 'true';
+
+    return { query, query_options, count };
+}
+
+module.exports = { parseHelper };
--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -28,3 +28,56 @@ export const deleteTask = async (req, res) => {
     //Works -> postman returned correct message
 };
 */
+
+// Best Google search that helped me understand MVC more
+// https://www.google.com/search?q=routes+controllers+and+models+style+in+javascrip&rlz=1C1GCEA_enUS1175US1175&oq=routes+controllers+and+models+style+in+javascrip&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRigATIHCAIQIRigATIHCAMQIRigAdIBCDg3NzhqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8
+
+const Task = require('../models/tasks.js');
+const User = require('../models/users.js');
+const { parseHelper } = require('../controllers/parseHelper.js');
+
+//First Get all tasks, respond with a list of tasks
+
+const tasksController = {
+
+    getTasks: async (req, res) => {
+        try {
+            const { query, query_options, count } = parseHelper(req);
+            
+            // Essentially if count is true we are told that 
+            // "return the count of documents that match the query  (instead of the documents themselves)".
+            if (count) {
+                const tasks = await Task.countDocuments(query);
+                return res.status(200).json({ message: "count", data: tasks });
+            }
+
+            const options = await Task.find(query, query_options.select, query_options)
+             .sort(query_options.sort)
+             .skip(query_options.skip)
+             .limit(query_options.limit);
+            res.status(200).json({ message: "success, following is the list of tasks", data: options });
+        } catch (error) {
+            //console.error('Error', error);
+            res.status(500).json({ message: 'An error occured', data: error.message });
+        }
+    },
+
+    postTask : async (req, res) => {
+        try {
+            const newTask = await Task.findById(req.query.taskId);
+            newTask = newTask.select(req.query.select || '');
+
+            if (!newTask) {
+                return res.status(404).json({ message: 'Task not found', data: null });
+            }
+            res.status(200).json({ message: 'success, following is the task', data: newTask });
+        } catch (error) {
+            //console.error('Error', error);
+            res.status(500).json({ message: 'An error occured', data: error.message });
+        }
+
+    };
+};
+
+
+
